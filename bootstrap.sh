0684325b83573314033a48a469c8f971ec08d184
yosqueoy <yosukeoyama@gmail.com>
2016-02-13 07:50:29 -0500

    updated bootstrap.sh for vim-plug

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
